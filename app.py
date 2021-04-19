edefb8e9adf9c41863fb67904b5fe0d2a853a4ff
Shubham Kumar <developer.shubham07@gmail.com>
2021-04-20 00:22:01 +0530

    COMPLETE migrating to marshmallow

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
 
 from ma import ma
 from db import db
 from blocklist import BLOCKLST
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -25,6 +28,11 @@ def create_tables():
     db.create_all()
 
 
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.message), 400
+
+
 jwt = JWTManager(app)
 
 
@@ -34,6 +42,10 @@ def check_if_token_in_blocklist(jwt_headers, jwt_payload):
     return jwt_payload["jti"] in BLOCKLST
 
 
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
